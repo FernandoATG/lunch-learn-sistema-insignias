0f61fb5b6a0fe53c7537f45d8c5d287fc6943102
Fernando De Jesus <fernandodejesus@auctiontechnologygroup.com>
2025-10-28 22:09:43 -0600

    Add test, CI and flake8

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from math import isclose
+import pytest
 from app import hello_world, add_numbers
 
 
@@ -18,4 +18,4 @@ def test_add_numbers():
 def test_add_numbers_with_floats():
     """Test add_numbers with float inputs."""
     assert add_numbers(1.5, 2.5) == 4.0
-    assert isclose(add_numbers(0.1, 0.2), 0.3, rel_tol=1e-9)
+    assert add_numbers(0.1, 0.2) == pytest.approx(0.3)
